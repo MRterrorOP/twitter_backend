b590021d2e5b90f017b5d9623fc376e247657608
Anuj Gupta <anujgupta4155@gmail.com>
2024-10-22 18:49:02 +0530

    fix:update code to access env Variable and log the if db connected or not

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import { createServer, IncomingMessage, ServerResponse } from "node:http";
 import * as dotenv from "dotenv";
 import requestHandler from "./app.js";
+import dbStatus from "./db/dbStatus.js";
 
-dotenv.config({ path: "../.env" });
-const hostname: string = process.env.HOSTNAME as string;
+dotenv.config({ path: ".env" });
+
+const hostname: string = process.env.APP_HOSTNAME as string;
 const port: string = process.env.PORT as string;
 
+//This tell us if server is connected with database or not
+const result = await dbStatus();
+console.log(result);
+
+//Server req handler
 createServer((req, res) => requestHandler(req, res)).listen(
   Number(port),
   hostname,
@@ -13,8 +20,3 @@ createServer((req, res) => requestHandler(req, res)).listen(
     console.log(`Server running at http://${hostname}:${port}/`);
   }
 );
-
-// (req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader("Content-Type", "text/plain");
-//   res.end("Hello World");}
